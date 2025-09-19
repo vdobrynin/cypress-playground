08058caf73b865cde8956340d39393e310a0df3a
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-09-18 22:06:06 -0700

    check boxes

--- cypress/e2e/ui-components.cy.js
+++ cypress/e2e/ui-components.cy.js
@@ -16,7 +16,7 @@ it('input fields', () => {
 
     cy.get('#inputEmail1').should('have.value', `${name}@test.com`).clear().type('test@bondaracademy.com')
     cy.get('#inputEmail1').should('not.have.value', '').clear().type('test@bondaracademy.com')
-       
+
     cy.get('#inputEmail1').should('not.have.value', '').clear().type('test@bondaracademy.com')
         .press(Cypress.Keyboard.Keys.TAB)
 
@@ -27,18 +27,34 @@ it('input fields', () => {
     cy.get('#input-password').type('Welcome{enter}')
 })
 
-    it.only('radio buttons', () => {
-        cy.contains('Forms').click()
-        cy.contains('Form Layouts').click()
+it('radio buttons', () => {
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
 
-        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(allRadioButtons => {
-            // cy.wrap(allRadioButtons).eq(0).check().should('be.checked') // --> don't work
-            cy.wrap(allRadioButtons).eq(0).check({ force: true }).should('be.checked')
-            cy.wrap(allRadioButtons).eq(1).check({ force: true })
-            cy.wrap(allRadioButtons).eq(0).should('not.be.checked')
-            cy.wrap(allRadioButtons).eq(2).should('be.disabled')
-        })
-        cy.contains('nb-card', 'Using the Grid').contains('Option 1').click() // or .check({ force: true })
-        cy.contains('nb-card', 'Using the Grid').contains('label', 'Option 2')
-            .find('input').check({ force: true })
+    cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(allRadioButtons => {
+        // cy.wrap(allRadioButtons).eq(0).check().should('be.checked') // --> don't work
+        cy.wrap(allRadioButtons).eq(0).check({ force: true }).should('be.checked')
+        cy.wrap(allRadioButtons).eq(1).check({ force: true })
+        cy.wrap(allRadioButtons).eq(0).should('not.be.checked')
+        cy.wrap(allRadioButtons).eq(2).should('be.disabled')
+    })
+    cy.contains('nb-card', 'Using the Grid').contains('Option 1').click() // or .check({ force: true })
+    cy.contains('nb-card', 'Using the Grid').contains('label', 'Option 2')
+        .find('input').check({ force: true })
+})
+
+it.only('checkboxes', () => {
+    cy.contains('Modal & Overlays').click()
+    cy.contains('Toastr').click()
+
+    // cy.get('[type="checkbox"]').check({ force: true }) // will not work 
+    // cy.get('[type="checkbox"]').uncheck({ force: true }) // will not work too
+    // cy.get('[type="checkbox"]').click({ force: true }) // will not work ether
+
+    cy.get('[type="checkbox"]').check({ force: true }) // 
+    cy.get('[type="checkbox"]').should('be.checked')   //
+
+    cy.get('[type="checkbox"]').eq(0).click({ force: true }) // uncheck
+    cy.get('[type="checkbox"]').eq(1).check({ force: true }) // check
+    cy.get('[type="checkbox"]').eq(2).uncheck({ force: true }) // uncheck another one
 })
